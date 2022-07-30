9dd99eb8a17b212ed2f054bf9f78af38cc895a1b
seenevz <sernevess@gmail.com>
2022-07-30 19:07:44 +0100

    Done with addons

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
-import { Object3D, Vector2, Vector3 } from 'three'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+import { Object3D, Vector2, Vector3 } from 'three';
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector<HTMLCanvasElement>('canvas.webgl')!
+const canvas = document.querySelector<HTMLCanvasElement>('canvas.webgl')!;
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Objects
@@ -22,130 +22,174 @@ const scene = new THREE.Scene()
 const object1 = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 16, 16),
   new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-object1.position.x = - 2
+);
+object1.position.x = -2;
 
 const object2 = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 16, 16),
   new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
+);
 
 const object3 = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 16, 16),
   new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-object3.position.x = 2
+);
+object3.position.x = 2;
 
-scene.add(object1, object2, object3)
+scene.add(object1, object2, object3);
 
 /**
-* Raycaster
-*/
+ * Raycaster
+ */
 
-const raycaster = new THREE.Raycaster()
-const cameraRaycaster = new THREE.Raycaster()
-const objectsToTest = [object1, object2, object3]
-const rayOrigin = new Vector3(-3, 0, 0)
-const rayDirection = new Vector3(1, 0, 0)
-rayDirection.normalize()
+let currentlySelected: Object3D | null = null;
 
-raycaster.set(rayOrigin, rayDirection)
+const raycaster = new THREE.Raycaster();
+const cameraRaycaster = new THREE.Raycaster();
+const objectsToTest = [object1, object2, object3];
+const rayOrigin = new Vector3(-3, 0, 0);
+const rayDirection = new Vector3(1, 0, 0);
+rayDirection.normalize();
 
-const resetSpheres = (spheresArray: Object3D[]) => spheresArray.forEach((obj) => obj.material.color.set('#ff0000'))
+raycaster.set(rayOrigin, rayDirection);
 
-const intersectSpheres = (intersectionsArr: THREE.Intersection<THREE.Object3D<THREE.Event>>[]) => intersectionsArr.forEach(({ object }) => object.material.color.set('#0000ff'))
+const resetSpheres = (spheresArray: Object3D[]) =>
+  spheresArray.forEach((obj) => obj.material.color.set('#ff0000'));
 
-const intersectMouse = (intersectionsArr: THREE.Intersection<THREE.Object3D<THREE.Event>>[], objectsToTest: Object3D[]) => {
-  objectsToTest.forEach(obj => {
-    if (intersectionsArr.find(intersect => intersect.object.id === obj.id)) obj.material.color.set('#00ff00')
-  })
-}
+const intersectSpheres = (
+  intersectionsArr: THREE.Intersection<THREE.Object3D<THREE.Event>>[]
+) =>
+  intersectionsArr.forEach(({ object }) =>
+    object.material.color.set('#0000ff')
+  );
+
+const intersectMouse = (
+  intersectionsArr: THREE.Intersection<THREE.Object3D<THREE.Event>>[],
+  objectsToTest: Object3D[]
+) => {
+  if (!intersectionsArr.length) currentlySelected = null;
+  else {
+    objectsToTest.forEach((obj) => {
+      if (
+        intersectionsArr.find((intersect) => intersect.object.id === obj.id)
+      ) {
+        obj.material.color.set('#00ff00');
+        currentlySelected = obj;
+      }
+    });
+  }
+};
 /**
-* Mouse
-*/
+ * Mouse
+ */
 
-const mouse = new Vector2()
+const mouse = new Vector2();
 
 window.addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX / sizes.width * 2 - 1
-  mouse.y = - (event.clientY / sizes.height) * 2 + 1
-})
+  mouse.x = (event.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+});
+
+window.addEventListener('click', (e) => {
+  if (currentlySelected) {
+    let name = null;
+
+    switch (currentlySelected.id) {
+      case object1.id:
+        name = 'object1';
+        break;
+      case object2.id:
+        name = 'object2';
+        break;
+      case object3.id:
+        name = 'object3';
+        break;
+    }
+
+    console.log('clicked on', name);
+  }
+});
 
 /**
  * Sizes
  */
 const sizes = {
   width: window.innerWidth,
-  height: window.innerHeight
-}
+  height: window.innerHeight,
+};
 
 window.addEventListener('resize', () => {
   // Update sizes
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
   // Update camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
   // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime();
 
   //spheres animation
 
-  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
-  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
-  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
 
   // Raycaster intersections
 
-  const intersects = raycaster.intersectObjects(objectsToTest)
-  cameraRaycaster.setFromCamera(mouse, camera)
-  const mouseIntersects = cameraRaycaster.intersectObjects(objectsToTest)
+  const intersects = raycaster.intersectObjects(objectsToTest);
+  cameraRaycaster.setFromCamera(mouse, camera);
+  const mouseIntersects = cameraRaycaster.intersectObjects(objectsToTest);
 
-  resetSpheres(objectsToTest)
-  intersectSpheres(intersects)
-  intersectMouse(mouseIntersects, objectsToTest)
+  resetSpheres(objectsToTest);
+  intersectSpheres(intersects);
+  intersectMouse(mouseIntersects, objectsToTest);
 
   // Update controls
-  controls.update()
+  controls.update();
 
   // Render
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 
   // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
-}
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
